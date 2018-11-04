ff4b3a3551cd998a0c0c11655cd18983606b7354
jx <33095108+JxBetter@users.noreply.github.com>
2018-11-04 15:14:16 +0800

    change ajax

--- server.js
+++ server.js
@@ -3,20 +3,32 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 
-var jsonParser = bodyParser.json();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });  
 
 var d = {};
 
 app.get('/', function (req, res) {
 
+	res.sendfile("./a.html");
+})
+
+
+app.post('/', urlencodedParser, function (req, res) {
+	d = req.body;
+	res.send(JSON.stringify({'result': 1, 'success': true}));
+})
+
+
+app.post('/ajax', urlencodedParser, function (req, res) {
+	console.log(req.body);
+	res.setHeader('Access-Control-Allow-Origin', 'null');
 	res.json(d);
 })
 
 
-app.post('/', jsonParser, function (req, res) {
-	d = req.body;
-	res.send(JSON.stringify({'result': 1, 'success': true}));
-})
- 
+app.post('/refresh', function (req, res) {
+	res.setHeader('Access-Control-Allow-Origin', 'null');
+	d = {};
+	res.json({success: true});
+}) 
 var server = app.listen(serverPort)
